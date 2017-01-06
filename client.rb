816fa7275399c43f40d4aa2d4b8e4ec9a85963d5
saikrishna337 <crazykrishna64@gmail.com>
2017-01-06 14:53:23 -0800

    Update client.rb

--- client.rb
+++ client.rb
@@ -1,8 +1,9 @@
-log_level        :info
-log_location     STDOUT
-chef_server_url  'https://api.chef.io/organizations/abdul'
-validation_client_name 'abdul-validator'
-validation_key '/etc/chef/abdul-validator.pem'
-client_key '/etc/chef/client.pem'
-ssl_verify_mode    :verify_none
-                             
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "localhost.localdomain"
+client_key               "~/chef-repo/.chef/krishna337.pem"
+chef_server_url          "https://manage.chef.io/organizations/patlolla"
+cookbook_path            ["~/chef-repo/.chef/../cookbooks"]
+ssl_verify_mode          :verify_none
+~
